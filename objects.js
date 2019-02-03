2ecee3ceff31a5b2ebdf44fc0fa48bdf61c9948a
Sherief Farid <sherief@farid.cloud>
2019-02-03 05:47:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,6 +11,6 @@ function updatePlaylist(playlist,artist,song){
 }
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
 }
